d5bfdb6059f74fb27e33d551bdd9eca460051246
prantik.sarmah <prantik.sarmah@IN-LAP-PRANTIK>
2018-07-16 11:57:28 +0530

    Adding projects

---
+++ ComponentOneControlPanel/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GrapeCity.Tools.ComponentOneControlPanel
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                try
+                {
+                    this.DragMove();
+                }
+                catch
+                {
+
+                }
+
+            }
+        }
+
+        private void CloseApp(object sender, RoutedEventArgs e)
+        {
+            App.Current.Shutdown();
+            Environment.Exit(0);
+        }
+
+        private void contactusblock_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+    }
+}
